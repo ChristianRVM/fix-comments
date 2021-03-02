226cc99fd40ae7f2a3a27dce05dd278844510b2a
Christian R. Villeda <70591939+ChristianRVM@users.noreply.github.com>
2021-03-01 23:02:25 -0600

    Initial Upload
    Se cargo el archivo PHP del plugin.

---
+++ fix-comments.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: Disqus Comments Preview Fix for TagDiv
+ * Version: 1.1
+ * Plugin URI: http://csharpspot.net/
+ * Description: Este plugin resulve los problemas de los comentarios en modo vista previa para algunos temas como NewsPaper y NewsMag.
+ * Author: Richard Watterson
+ * Author URI: https://crvmdev.net/
+ * Requires at least: 5.0
+ * Tested up to: 5.6.2
+ *
+ * Text Domain: fix-comments
+ * Domain Path: /
+ *
+ * @package WordPress
+ * @author Richard Watterson
+ * @since 1.1
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+add_filter( 'comments_template', function ( $template ) {
+    if (is_preview()){
+        return ;
+    }
+	return $template;
+},999);
+
